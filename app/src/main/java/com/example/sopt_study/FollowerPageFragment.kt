4736a60bfddb0099564e89a174161c87eb1c481c
Choi Woo hyeong <gua05093@gmail.com>
2022-04-18 14:12:50 +0900

    worked on desktop

--- app/src/main/java/com/example/sopt_study/FollowerPageFragment.kt
+++ app/src/main/java/com/example/sopt_study/FollowerPageFragment.kt
@@ -1,15 +1,19 @@
 package com.example.sopt_study
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.sopt_study.databinding.FragmentPageFollowerBinding
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sopt_study.databinding.FragmentFollowerPageBinding
 
 
 class FollowerPageFragment : Fragment() {
-    private var _binding: FragmentPageFollowerBinding? = null
+    private lateinit var followerAdapter: FollowerAdapter
+    private var _binding: FragmentFollowerPageBinding? = null
     private val binding get() = _binding!!
 
 
@@ -17,12 +21,38 @@ class FollowerPageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentPageFollowerBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentFollowerPageBinding.inflate(layoutInflater, container, false)
+
+
+
+        initAdapter()
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initAdapter(){
+        val myDecoration = MyDecoration(200f, 200f, Color.BLUE)
+
+        followerAdapter = FollowerAdapter()
+        binding.rvFollower.adapter = followerAdapter
+        binding.rvFollower.addItemDecoration(myDecoration)
+
+
+
+        followerAdapter.userList.addAll(
+            listOf(
+                UserData("이강민", "1가나다라마바사아자차카타파하2가나다라마바사아자차카타파하3가나다라마바사아자차카타파하4가나다라마바사아자차카타파하5가나다라마바사아자차카타파하6"),
+                UserData("이강민", "안드로이드 파트장"),
+                UserData("이강민", "안드로이드 파트장"),
+                UserData("이강민", "안드로이드 파트장"),
+                UserData("이강민", "안드로이드 파트장"),
+                UserData("이강민", "안드로이드 파트장")
+            )
+        )
+        followerAdapter.notifyDataSetChanged()
     }
 }
--- app/src/main/java/com/example/sopt_study/RepositoryPageFragment.kt
+++ app/src/main/java/com/example/sopt_study/RepositoryPageFragment.kt
@@ -5,11 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.sopt_study.databinding.FragmentPageRepositoryBinding
+import com.example.sopt_study.databinding.FragmentRepositoryPageBinding
 
 
 class RepositoryPageFragment : Fragment() {
-    private var _binding: FragmentPageRepositoryBinding? = null
+    private lateinit var repositoryAdapter: RepositoryAdapter
+    private var _binding: FragmentRepositoryPageBinding? = null
     private val binding get() = _binding!!
 
 
@@ -17,12 +18,31 @@ class RepositoryPageFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentPageRepositoryBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentRepositoryPageBinding.inflate(layoutInflater, container, false)
+
+        initAdapter()
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initAdapter(){
+        repositoryAdapter = RepositoryAdapter()
+        binding.rvRepository.adapter = repositoryAdapter
+
+        repositoryAdapter.userList.addAll(
+            listOf(
+                UserData("이강민", "1가나다라마바사아자차카타파하2가나다라마바사아자차카타파하3가나다라마바사아자차카타파하4가나다라마바사아자차카타파하5가나다라마바사아자차카타파하6"),
+                UserData("이강민", "안드로이드 파트장"),
+                UserData("이강민", "안드로이드 파트장"),
+                UserData("이강민", "안드로이드 파트장"),
+                UserData("이강민", "안드로이드 파트장"),
+                UserData("이강민", "안드로이드 파트장")
+            )
+        )
+        repositoryAdapter.notifyDataSetChanged()
     }
 }
