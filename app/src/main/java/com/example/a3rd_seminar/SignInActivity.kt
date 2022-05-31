37ce2c823d7757923ceae04e0fd8a7bc9dc48c45
Choi Woo hyeong <gua05093@gmail.com>
2022-05-31 14:51:05 +0900

    [FEAT]

--- app/src/main/java/com/example/a3rd_seminar/SignInActivity.kt
+++ app/src/main/java/com/example/a3rd_seminar/SignInActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.a3rd_seminar.data.SOPTSharedPreferences
 import com.example.a3rd_seminar.databinding.ActivitySignInBinding
 import com.example.a3rd_seminar.sever_tools.RequestSignIn
 import com.example.a3rd_seminar.sever_tools.ResponseSignIn
@@ -17,16 +18,18 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignInBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
+        SOPTSharedPreferences.init(this)
+        isAutoLogin()
         initEvent()
-        initButtonClickListener()
-        binding.rbAutoSignIn.isSelected = true
+        initClickEvent()
 
         binding.btSignUp.setOnClickListener {
             val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
         }
+
+        setContentView(binding.root)
     }
 
     private fun initEvent() {
@@ -40,6 +43,12 @@ class SignInActivity : AppCompatActivity() {
             id = binding.etGithubId.text.toString(),
             password = binding.etPassword.text.toString()
         )
+
+        SOPTSharedPreferences.setUserData(
+            id = binding.etGithubId.text.toString(),
+            passWord = binding.etPassword.text.toString()
+        )
+
         val call: Call<ResponseSignIn> = ServiceCreator.soptService.signIn((requestSignIn))
         call.enqueueUtil(
             onSuccess = {
@@ -56,9 +65,37 @@ class SignInActivity : AppCompatActivity() {
         )
     }
 
-    private fun initButtonClickListener() {
+    private fun initClickEvent() {
         binding.rbAutoSignIn.setOnClickListener {
             binding.rbAutoSignIn.isSelected = !binding.rbAutoSignIn.isSelected
+
+            SOPTSharedPreferences.setAutoLogin(binding.rbAutoSignIn.isSelected)
+        }
+    }
+
+    private fun isAutoLogin() {
+        if (SOPTSharedPreferences.getAutoLogin()) {
+
+            val requestSignIn = RequestSignIn(
+                id = SOPTSharedPreferences.getUserID(),
+                password = SOPTSharedPreferences.getUserPassWord()
+            )
+
+            val call: Call<ResponseSignIn> = ServiceCreator.soptService.signIn((requestSignIn))
+            call.enqueueUtil(
+                onSuccess = {
+                    Toast.makeText(
+                        this@SignInActivity,
+                        "${it.data.email}님 반갑습니다! 자동 로그인 되었습니다.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    startActivity(Intent(this@SignInActivity, HomeActivity::class.java))
+                },
+                onError = {
+                    Toast.makeText(this@SignInActivity, "로그인에 실패하셨습니다.", Toast.LENGTH_SHORT).show()
+                }
+            )
+            finish()
         }
     }
 }
--- app/src/main/java/com/example/a3rd_seminar/data/SOPTSharedPreferences.kt
+++ app/src/main/java/com/example/a3rd_seminar/data/SOPTSharedPreferences.kt
@@ -6,6 +6,9 @@ import android.content.SharedPreferences
 object SOPTSharedPreferences {
     private const val STORAGE_KEY = "USER_AUTH"
     private const val AUTO_LOGIN = "AUTO_LOGIN"
+    private const val ID = "ID"
+    private const val PASS_WORD = "PASS_WORD"
+
     private lateinit var preferences: SharedPreferences
 
     fun init(context: Context) {
@@ -20,6 +23,19 @@ object SOPTSharedPreferences {
         preferences.edit()
             .putBoolean(AUTO_LOGIN, value)
             .apply()
+    }
+
+    fun setUserData(id: String, passWord: String) {
+        preferences.edit().putString(ID, id).apply()
+        preferences.edit().putString(PASS_WORD, passWord).apply()
+    }
+
+    fun getUserID(): String {
+        return preferences.getString(ID, "").toString()
+    }
+
+    fun getUserPassWord(): String {
+        return preferences.getString(PASS_WORD, "").toString()
     }
 
     fun setLogout(context: Context) {
